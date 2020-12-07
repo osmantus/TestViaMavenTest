da2a351e7d32529bc34bab862eb1bfc513671a7a
Alex <poznanski.alex@gmail.com>
2020-12-08 00:17:58 +0200

    commit to master on 2020-12-08

--- src/main/java/testSuites/SiteMainPageLogic.java
+++ src/main/java/testSuites/SiteMainPageLogic.java
@@ -1,10 +1,10 @@
 package testSuites;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 
-import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -24,7 +24,7 @@ public class SiteMainPageLogic {
     }
 
     public SiteMainPageLogic categoriesCallingClick() {
-        $$(categories).shouldHave(size(1));
+        $$(categories).shouldHave(CollectionCondition.sizeNotEqual(0));
         $(categories)
                 .shouldBe(visible)
                 .click();
