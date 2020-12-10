63afbc611eeb14ba4bf37133ac4ca671c09fa434
Alex <poznanski.alex@gmail.com>
2020-12-10 07:40:53 +0200

    commit to master on 2020-12-10-004

--- src/test/java/testSuites/WebSiteTest.java
+++ src/test/java/testSuites/WebSiteTest.java
@@ -10,6 +10,7 @@ public class WebSiteTest {
 
     @BeforeMethod
     public void initTest() {
+        System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver\\chromedriver.exe");
         Configuration.startMaximized = true;
         Configuration.timeout = 10000;
         open("https://rozetka.com.ua/");
